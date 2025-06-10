8407639cf10409401e92d1515015df0de78641f9
Slawomir <slawomir_pacek@op.pl>
2025-06-10 13:12:17 +0200

    lista9

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -33,9 +33,7 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void addAll(List<T> list) throws Exception {
-        if (list == null) {
-            throw NullPointerException;
-        }
+        if (list == null) throw NullPointerException;
         for (int i = 0; i < list.size(); i++) {
             add(list.get(i));
         }
