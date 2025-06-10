2f8ecc852f57d653d4f2eac33e9211e0cca0b4db
Slawomir <slawomir_pacek@op.pl>
2025-06-10 14:30:28 +0200

    lista11

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -5,10 +5,10 @@ import java.util.NoSuchElementException;
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
     private static final double GROWTH_FACTOR = 1.5;
+    private Exception NullPointerException;
 
     private T[] elements;
     private int size;
-    public Exception NullPointerException;
 
     @SuppressWarnings("unchecked")
     public ArrayList() {
